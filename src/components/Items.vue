9ac5b4801208d036e7d6838db3cfa8502f094579
Normaneil E. Macutay <normaneil.macutay@gmail.com>
2022-03-26 12:57:10 +0800

    Added table list of ip address

---
+++ src/components/Items.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <form class="w-full max-w-lg">
+      <div class="flex -mx-3 mb-6 ">
+        <div class="w-full px-3 mb-6 md:mb-0">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-first-name"
+          >
+            IP ADDRESS
+          </label>
+          <input
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id="grid-first-name"
+            type="text"
+            v-model="form.ip_add"
+            placeholder="127.0.0.1"
+          />
+        </div>
+        <div class="w-full  px-1">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-last-name"
+          >
+            LABEL
+          </label>
+          <input
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-last-name"
+            type="text"
+            v-model="form.label"
+            placeholder="localhost"
+          />
+        </div>
+        <div class="w-full md:w-1/2 px-4">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-last-name"
+          >
+            &nbsp;
+          </label>
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-3 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+            type="button"
+            @click="addIpAddress()"
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    </form>
+
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <thead
+        class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+      >
+        <tr>
+          <th scope="col" class="px-6 py-3">
+            IP
+          </th>
+          <th scope="col" class="px-6 py-3">
+            Label
+          </th>
+          <th scope="col" class="px-6 py-3">
+            <span class="sr-only">Edit</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="task in items"
+          :key="task.id"
+          class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+        >
+          <th
+            scope="row"
+            class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+          >
+            {{ task.ip_add }}
+          </th>
+          <td class="px-6 py-4">
+            {{ task.label }}
+          </td>
+
+          <td class="px-6 py-4 text-right">
+            <a
+              href="#"
+              class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+              >View</a
+            >
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div
+      id="toast-success"
+      class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800 absolute top-5 right-5"
+      role="alert"
+      v-if="ifAdded"
+    >
+      <div
+        class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200"
+      >
+        <svg
+          class="w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </div>
+      <div class="ml-3 text-sm font-normal">Item added successfully.</div>
+      <button
+        type="button"
+        class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+        data-dismiss-target="#toast-success"
+        aria-label="Close"
+      >
+        <span class="sr-only">Close</span>
+        <svg
+          class="w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+import api from "@/api";
+export default {
+  data() {
+    return {
+      items: [],
+      form: {
+        ip_add: "",
+        label: "",
+      },
+      ifAdded: false,
+    };
+  },
+  mounted() {
+    this.getIpAddresses();
+  },
+  methods: {
+    async getIpAddresses() {
+      const result = await api.getIpAddresses();
+      console.log(result.data.data);
+      this.items = result.data.data.length > 0 ? result.data.data : [];
+    },
+    async addIpAddress() {
+      const result = await api.addIpAddress(this.form);
+      console.log(result.data);
+      if (result.data.success) {
+        this.ifAdded = true;
+        this.form.ip_add = "";
+        this.form.label = "";
+        this.getIpAddresses();
+
+        setTimeout(() => {
+          this.ifAdded = false;
+        }, 2000);
+      } else {
+        alert(JSON.stringify(result.data.message));
+      }
+    },
+  },
+};
+</script>
